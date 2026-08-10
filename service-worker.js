ed5b24f98f4d40811fc64fb799001976d7867605
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-11 02:01:13 +0300

    P51144

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-54-25-p51143-appointments-time-team-columns";
+const CACHE_VERSION = "petatoe-pwa-18-54-26-p51144-scheduling-driver-groomer";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
