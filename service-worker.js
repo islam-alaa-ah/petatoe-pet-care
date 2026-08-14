4196cfa349575ae5dad5ce9893f86c2fa995468e
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-14 17:30:26 +0300

    P5.11.24 — Mobile Tables & Icons Pilot

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-54-78-p51123-desktop-header-sidebar-light-dark";
+const CACHE_VERSION = "petatoe-pwa-18-54-79-p51124-mobile-tables-icons-pilot";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
