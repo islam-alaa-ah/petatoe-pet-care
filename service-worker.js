142c6a42d29e536dd6d43d56d4750bb5500f10c2
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-16 00:52:13 +0300

    Force Latin digits in appointment numeric inputs

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-54-95-service-dropdown-edit-hydration";
+const CACHE_VERSION = "petatoe-pwa-18-54-96-appointment-latin-numeric-inputs";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
