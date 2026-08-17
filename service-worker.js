ea15ad3963985aab42527cc5561c5094fb555588
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-17 14:22:32 +0300

    Compact appointment PDF timelines and add execution duration under services

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-55-33-installation-summary-enterprise-pdf";
+const CACHE_VERSION = "petatoe-pwa-18-55-34-compact-appointment-timeline-pdf";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
