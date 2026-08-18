e0f019cb47a9bc2f266fbb0a4ef3579811c9a347
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-18 23:46:06 +0300

    P5.13.8.32

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-55-51-daily-sales-invoices-summary-report";
+const CACHE_VERSION = "petatoe-pwa-18-55-52-sales-invoice-block-pdf-layout";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
