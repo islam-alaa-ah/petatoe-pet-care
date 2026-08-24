c3308f889333c85d96101c2d0cc7c615d8afa82f
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-25 02:41:49 +0300

    p5.13.8.72-r10-payroll-top

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-56-09-salary-commission-period-adjustment-items-r9-p5-13-8-71";
+const CACHE_VERSION = "petatoe-pwa-18-56-10-payroll-header-layout-translation-cleanup-r10-p5-13-8-72";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
