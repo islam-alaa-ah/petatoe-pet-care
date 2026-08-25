b643f7f2a2891ac5f3c846ff2f2e6c882e05c51b
islam-alaa-ahmed <islam.alaa9493@gmail.com>
2026-08-26 01:00:05 +0300

    Certify Payroll and SEA VIBE mobile dark mode and modernize payroll month card

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "petatoe-pwa-18-56-14-payroll-seavibe-dark-mode-certification-r15-p5-13-8-72";
+const CACHE_VERSION = "petatoe-pwa-18-56-15-payroll-seavibe-mobile-dark-certification-r16-p5-13-8-72";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const VENDOR_CACHE = `${CACHE_VERSION}-vendor`;
